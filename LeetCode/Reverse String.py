bd7bbd0f7ee1a8866f96ae3bad3f268c4556d12d
Jeremy Zheng <DataMonk2017@users.noreply.github.com>
2017-03-26 22:47:08 -0400

    Update Reverse String.py

--- LeetCode/Reverse String.py
+++ LeetCode/Reverse String.py
@@ -19,5 +19,5 @@ class Solution(object):
         
         #pythonic way
         return ''.join([s[-(i+1)] for i in range(len(s))])
-        #second way
+        #second way: Extened Slices
         return s[::-1]
