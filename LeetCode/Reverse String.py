c297bdaed4df5d793e3c0c666b6345391aa374f7
Jeremy Zheng <DataMonk2017@users.noreply.github.com>
2017-03-26 22:42:18 -0400

    Create Reverse String.py

---
+++ LeetCode/Reverse String.py
@@ -0,0 +1,23 @@
+#Write a function that takes a string as input and returns the string reversed.
+
+#Example:
+#Given s = "hello", return "olleh".
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        #dynamic programming
+        def fun1(s):
+            if len(s) == 0:
+                return ''
+            else:
+                return s[-1]+fun1(s[:-1])
+        return fun1(s)   
+        
+        #pythonic way
+        return ''.join([s[-(i+1)] for i in range(len(s))])
+        #second way
+        return s[::-1]
